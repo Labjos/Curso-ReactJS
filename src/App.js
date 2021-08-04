da610970f390ae964d3c53659e22f45318166824
Labjos <info@mundosurnatural.com.ar>
2021-08-04 16:25:45 -0300

    Entrega Dos NavBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logoBR.png';
 import './App.css';
+import {NavBar } from './components/navBar.jsx'
 
 function App() {
+  
   return (
+   
     <div className="App">
       <header className="App-header">
+
+      <NavBar />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Bienvenido a Black#53Red el SpeackEasy Global ;)        </p>
+         Bienvenido a Black#53Red el SpeackEasy Global !!!</p>
         
       </header>
-    </div>
-  );
-}
+    </div>)
+  }
 
 export default App;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import'./navBar.css'
+
+export const NavBar =() => {
+
+  return (
+    <div className="navBar">
+            <nav id="menuPrincipal">
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Productos</li>
+                        <li>Categorias</li>
+                        <li>Compra</li>
+                        <li>Carga</li>
+                        <li>Detalles</li>
+                        <li>Contacto</li>
+                    </ul>
+            </nav>
+    </div>
+  )
+} 
+    
